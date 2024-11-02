7f480b7d9cb198297cf69fe4332c230f06ae6666
Meng Demao <mengdemao19951021@163.com>
2024-11-02 12:37:28 +0800

    add github action

--- compiler.sh
+++ compiler.sh
@@ -11,6 +11,14 @@ export RTMES_ARCH=$1
 export RTEMS_VERSION=$2
 export PREFIX_PATH=${INSTALL_PATH}/rtems-${RTMES_ARCH}
 
-pushd rtems/rsb/rtems >>/dev/null || exit
-../source-builder/sb-set-builder --prefix=${PREFIX_PATH} ${RTEMS_VERSION}/rtems-${RTMES_ARCH}.bset --jobs=$(nproc)
-popd >>/dev/null || exit
+# pushd rtems/rsb/rtems >>/dev/null || exit
+# ../source-builder/sb-set-builder --prefix=${PREFIX_PATH} ${RTEMS_VERSION}/rtems-${RTMES_ARCH}.bset --jobs=$(nproc)
+# popd >>/dev/null || exit
+
+pushd ${PREFIX_PATH}
+
+find bin -type f -executable | xargs strip --strip-unneeded
+find libexec -type f -executable | xargs strip --strip-unneeded
+find ${RTMES_ARCH}-rtems6 -type f -executable | xargs strip --strip-unneeded
+
+popd
--- download.sh
+++ download.sh
@@ -6,6 +6,6 @@ git clone --depth=1 https://github.com/RTEMS/rtems-tools.git rtems/tools
 git clone --recurse-submodules --shallow-submodules https://github.com/RTEMS/rtems-libbsd.git rtems/libbsd
 git clone --recurse-submodules --shallow-submodules https://github.com/RTEMS/rtems-examples.git rtems/examples
 
-pushd rtems/examples >> /dev/null | exit
+pushd rtems/examples
 git apply ../../patch/examples.patch
-popd >> /dev/null | exit
+popd
