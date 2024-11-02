530c666545d9c3f8c20e811a3cadd2648c0abaa8
Meng Demao <mengdemao19951021@163.com>
2024-11-02 10:49:16 +0800

    add apply patch

--- download.sh
+++ download.sh
@@ -5,3 +5,7 @@ git clone --depth=1 https://github.com/RTEMS/rtems.git rtems/rtems
 git clone --depth=1 https://github.com/RTEMS/rtems-tools.git rtems/tools
 git clone --recurse-submodules --shallow-submodules https://github.com/RTEMS/rtems-libbsd.git rtems/libbsd
 git clone --recurse-submodules --shallow-submodules https://github.com/RTEMS/rtems-examples.git rtems/examples
+
+pushd rtems/examples >> /dev/null | exit
+git apply ../../patch/examples.patch
+popd >> /dev/null | exit
